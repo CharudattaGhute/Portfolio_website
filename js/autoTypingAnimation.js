b6f4b077503bdc8815135bb6480416498f3aec37
CharudattaGhute <ghutecharudatta@gmail.com>
2024-08-06 11:42:16 +0530

    updted anime

--- js/autoTypingAnimation.js
+++ js/autoTypingAnimation.js
@@ -1,10 +1,9 @@
-setTimeout(function() {
-    $('.about-info-inner').typed({
-      strings: [
-        "<span>Hi, I'm Charudatta Ghute,</span> a passionate and creative web developer with a love for turning ideas into beautifully crafted websites",
-        
-      ],
-      typeSpeed: 10,
-      contentType: 'html'
-    });
-  }, 100);
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      "<span>Hi, I'm Charudatta Ghute,</span> A passionate and creative MERN stack developer with a love for turning ideas into beautifully crafted web applications.",
+    ],
+    typeSpeed: 5,
+    contentType: "html",
+  });
+}, 100);
